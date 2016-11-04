0b5f8320d917edfbc586704a8cfede239a4988a2
Surya Teja K <shanmukhateja@gmail.com>
2016-11-04 23:12:32 +0530

    added repo lang;repo descr

--- gtrends/gtrends.py
+++ gtrends/gtrends.py
@@ -19,20 +19,42 @@ b=BeautifulSoup(a,"html.parser")
 
 # Acc. to Source code, the repo list start from 'ol' with class 'repo-list'
 c=b.find("ol",class_="repo-list") 
+
 print("Parsing..")
+
 # Next, we find all the children elements to 'ol'
 d=c.findAll("li",class_="col-12 d-block width-full py-4 border-bottom")
 
 # Iterate this list and print required data
 for x in d:
+        
+        # Get the name of the repo
+        name=x.contents[1].find("a").text.strip()
+
+        # Get the description of the repo
+        descr = x.contents[5].text.strip()
 
         # Split this command into multiple parts to understand it properly
         url=base_url+x.contents[1].find("a").attrs.get("href")
-        
-        # This way is simpler 
-        name=url.split("/")[-1]
 
+        # Get the programming language used for the repo
+        prog_lang=x.contents[7].find("span",class_="mr-3")
+
+        # There are a few cases where a project has no specific
+        # programming language. Hence show it when available
+        if(type(prog_lang) != type(None)):
+                prog_lang=prog_lang.text.strip()
+
+        
+        
         # Finally print the details
-        print("Repo name: "+name)
-        print("Repo URL: "+url)
-        print("\n\n")
+        print("\n")
+        print("Repo name   : "+name)
+        print("Repo desc   : "+descr)
+        print("Repo URL    : "+url)
+        
+        # Check if programming language exists
+        if(type(prog_lang) != type(None)):
+           print("Repo Lang.  : "+prog_lang)
+        else:
+                print("Repo Lang.  : --")
